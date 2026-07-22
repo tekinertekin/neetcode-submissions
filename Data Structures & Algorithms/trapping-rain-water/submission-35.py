3a009401a05c0cd13f0e9f96bb3ec4460f16fa25
Tekin Ertekin <tekinertekin@gmail.com>
2026-07-22 15:49:19 +0300

    Add: trapping-rain-water - submission-35

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-35.py
@@ -0,0 +1,34 @@
+class Solution:
+    def _helper(self, height: List[int]) -> int:
+        if not height:
+            return 0
+        global_max = height[0]
+        global_max_idx = 0
+        area = 0
+        local_max = 0
+        local_max_idx = 0
+        minus_area = 0
+        for i in range(0,len(height)):
+            if height[i] >= local_max:
+                if local_max:
+                    area += ((i-local_max_idx) * local_max) - minus_area
+                local_max = height[i]
+                minus_area = 0
+                local_max_idx = i
+            minus_area += height[i]
+        return area
+
+
+    def trap(self, height: List[int]) -> int:
+        global_max = 0
+        global_max_idx = 0
+        area = 0
+        for i in range(0, len(height)):
+            if height[i] >= global_max:
+                global_max = height[i]
+                global_max_idx = i
+        area += self._helper(height[0:global_max_idx + 1])
+        area += self._helper(height[global_max_idx:len(height)][::-1])
+        return area
+
+        
