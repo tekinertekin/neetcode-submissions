6a94b8ad5b24a89d2ffc7c3211fbb2c36f7c071d
Tekin Ertekin <tekinertekin@gmail.com>
2026-08-03 19:16:14 +0300

    Add: search-2d-matrix - submission-13

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-13.py
@@ -0,0 +1,19 @@
+class Solution:
+    def _helper(self, pos: int, width: int) -> [int, int]:
+        return [pos%width, pos // width]
+
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        width = len(matrix[0])
+        l = 0
+        r = (len(matrix) * width) - 1
+        while l <= r:
+            mid = l + ((r - l) // 2)
+            col, row = self._helper(mid, width)
+            if matrix[row][col] > target:
+                r = mid - 1
+            elif matrix[row][col] < target:
+                l = mid + 1
+            else:
+                return True
+        return False
+        
